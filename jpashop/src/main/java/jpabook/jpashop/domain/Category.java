2c1b8a15bace9943eece4ecd96044805cf6925b4
박지수 Jisu Park <qkrwltn8976@dongguk.edu>
2022-03-16 21:51:54 +0900

    엔티티 설계 시 주의점

--- jpashop/src/main/java/jpabook/jpashop/domain/Category.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Category.java
@@ -26,10 +26,16 @@ public class Category {
     private List<Item> items = new ArrayList<>();
 
     // 셀프로 양방향 연관관계 설정
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "parent_id")
     private Category parent;
 
     @OneToMany(mappedBy = "parent")
     private List<Category> child = new ArrayList<>();
+
+    //==연관관계 메서드==//
+    public void addChildCategory(Category child) {
+        this.child.add(child);
+        child.setParent(this);
+    }
 }
--- jpashop/src/main/java/jpabook/jpashop/domain/Order.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -18,14 +18,14 @@ public class Order {
     private long id;
 
     // order가 연관관계의 주인
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
 
-    @OneToMany(mappedBy = "order")
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
     private List<OrderItem> orderItems = new ArrayList<>();
 
-    @OneToOne // 자주 사용하는 테이블에 FK 설정, 연관관계의 주인
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL) // 자주 사용하는 테이블에 FK 설정, 연관관계의 주인
     @JoinColumn(name = "delivery_id")
     private Delivery delivery;
 
@@ -33,4 +33,20 @@ public class Order {
     private LocalDateTime orderDate;
 
     private OrderStatus status; // 주문상태 (ORDER, CANCEL)
+
+    //==연관관계 메서드==/
+    public void setMember(Member member) {
+        this.member = member;
+        member.getOrders().add(this);
+    }
+
+    public void addOrderItem(OrderItem orderItem) {
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public void setDelivery(Delivery delivery) {
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
 }
