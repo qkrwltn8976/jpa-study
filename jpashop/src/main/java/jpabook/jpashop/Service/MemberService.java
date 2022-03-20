ee134575bc9330639179575007ba3f2e6ab002a1
박지수 Jisu Park <qkrwltn8976@dongguk.edu>
2022-03-20 12:36:08 +0900

    회원 도메인 개발

---
+++ jpashop/src/main/java/jpabook/jpashop/Service/MemberService.java
@@ -0,0 +1,45 @@
+package jpabook.jpashop.Service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true) // 읽기에는 옵션 추가 필요
+@RequiredArgsConstructor // final 필드로 생성자 만들어줌 (@Autowired 필요없음)
+public class MemberService {
+
+    private final MemberRepository memberRepository; // final 어노테이션 사용 시 생성자 없으면 오류
+
+    /**
+     * 회원 가입
+     */
+    @Transactional // 쓰기에는 옵션추가하지 않기 (우선권)
+    public Long join(Member member) {
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+        if (!findMembers.isEmpty()) {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+    }
+
+    /**
+     * 회원 전체 조회
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Member findOne(Long memberId) {
+        return memberRepository.findOne(memberId);
+    }
+}
---
+++ jpashop/src/test/java/jpabook/jpashop/Service/MemberServiceTest.java
@@ -0,0 +1,61 @@
+package jpabook.jpashop.Service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class) // junit 실행 시 스프링과 엮어 실행
+@SpringBootTest // 스프링부트를 띄운 상태에서 테스트하고 싶을 때 사용
+@Transactional // 테스트 끝나면 롤백
+public class MemberServiceTest {
+
+    @Autowired
+    MemberService memberService;
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Autowired
+    EntityManager em;
+
+    @Test
+//    @Rollback(false) // 롤백하지 않고 커밋하여 insert 쿼리 볼 수 있음, 없으면 transactional 어노테이션으로 인해 롤백됨
+    public void join() throws Exception {
+        // given
+        Member member = new Member();
+        member.setName("kim");
+
+        // when
+        Long savedId = memberService.join(member);
+
+        // then
+        em.flush(); // db에 쿼리 실행하여 insert문 볼 수 있음
+        assertEquals(member, memberRepository.findOne(savedId));
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void validateDuplicateMember() throws Exception {
+        // given
+        Member member1 = new Member();
+        member1.setName("kim");
+
+        Member member2 = new Member();
+        member2.setName("kim");
+        // when
+        memberService.join(member1);
+        memberService.join(member2); // 예외가 발생해야 함
+
+        // then
+        Assert.fail("예외가ㅏ 바라생해야 한다.");
+    }
+}
